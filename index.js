a2e5f367704b27de582c2fc686450c4f31899e98
IcesyWisy <91879271+IceSangmalai@users.noreply.github.com>
2022-06-14 13:29:52 +0200

    add commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents, ClientPresence, UserFlags } = require('discord.js');
+const { Client, Intents, ClientPresence, UserFlags, Options, MessageSelectMenu } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -26,5 +26,20 @@ client.on("ready", () => {
     
 
 })
+
+client.on('interactionCreate', async interaction => {
+
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+        await interaction.reply(`Pong! That took you ${client.ws.ping} ms.`);
+	} else if (interaction.commandName === 'user') {
+        await interaction.reply(`Your amazing profile picture` + `${interaction.user.displayAvatarURL(true)}`);
+
+
+    } else if (interaction.commandName === 'server') {
+        
+    }
+});
 
 client.login(token);
